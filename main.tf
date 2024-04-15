94e5a59dfd81c7d52dbbddefd22bf53464ec4f51
Danish Niyaz <danish.niyaz@socar.my>
2024-04-15 13:42:41 +0800

    Instance creation using Foreach loop with locals

--- main.tf
+++ main.tf
@@ -10,3 +10,43 @@ resource "aws_vpc" "test_main" {
     Name = "main_terraform_vpc"
   }
 }
+
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+locals {
+  instances = {
+    "testnode-1" = {
+      region      = "ap-south-1"
+      instance_type = "t3.micro"
+    },
+    "testnode-2" = {
+      region      = "ap-south-1"
+      instance_type = "t3.micro"
+    },
+    "testnode-3" = {
+      region      = "ap-south-1"
+      instance_type = "t3.micro"
+    }
+
+  }
+}
+
+
+
+resource "aws_instance" "for_each-instances" {
+  for_each = local.instances
+  tags = {
+    Name = each.key
+      }
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = each.value.instance_type
+  
+}
